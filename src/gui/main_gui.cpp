5bb0993d2104b972423e714cb85adc13635ae4d2
horsicq <horsicq@gmail.com>
2024-04-17 08:38:19 +0200

    Update file(s): 2024-04-17

--- src/gui/main_gui.cpp
+++ src/gui/main_gui.cpp
@@ -25,6 +25,7 @@
 
 int main(int argc, char *argv[])
 {
+    // TODO move to XOptions
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
